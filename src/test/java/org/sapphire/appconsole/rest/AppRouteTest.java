bada8d39ad41e72a303fd539cb71aecf57fa8213
deepak <dsriniv8@asu.edu>
2017-07-20 18:21:28 -0700

    #The changes for the Junit/integration test cases has been done.

--- src/test/java/org/sapphire/appconsole/rest/AppRouteTest.java
+++ src/test/java/org/sapphire/appconsole/rest/AppRouteTest.java
@@ -2,6 +2,9 @@ package org.sapphire.appconsole.rest;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,15 +24,85 @@ public class AppRouteTest extends JerseyTest {
     }
 
     @Before
-    public void initialize() {
-    		webResource = client().resource(TestUtils.readPropertyFile("baseurl"));
+    public void initialize() throws Exception {
+    		try{
+    			webResource = client().resource(TestUtils.readPropertyFile("baseurl"));
+    		}
+    		catch(Exception e)
+    		{
+    			throw e;
+    		}
     }
- 
+    
+    //This method is for testing the app search functionality.
     @Test
-    public void testFetchAll() {
-		ClientResponse output = webResource.path("/appconsole-api/rest/widget/search").head();
-		System.out.print("hello::"+output.getEntity(String.class));
+    public void appSearchTest() {
+		ClientResponse output = webResource.path("/appconsole-api/rest/app/search").head();
         assertEquals("should return status 200", 200, output.getStatus());
     }
-
+    
+    //This method is for testing the app save functionality.
+    @Test
+    public void appSaveTest() throws Exception {
+    	
+    	String appSaveTestjson = "";
+    	ClassLoader classLoader = getClass().getClassLoader();
+    	
+    	//Reading JSON File for the put body. 
+    	try 
+    	{
+    		appSaveTestjson = IOUtils.toString(classLoader.getResourceAsStream("appSave.json"));
+    	} 
+    	catch (IOException e) 
+    	{
+    		//oops something bad happened :(
+    		e.printStackTrace();
+    		throw e;
+    	}
+    	
+    	if(appSaveTestjson == null && appSaveTestjson.isEmpty())
+    	{
+    		//oops something bad happened :(
+    		throw new Exception("Unexpected Error while reading appSave json file");
+    	}
+    	
+    	//Gonna make the PUT request.
+    	ClientResponse output = null;
+	output = webResource.path("/appconsole-api/rest/app/b594afad-1e6c-42c4-d58c-715c62cfdfc").type("application/json").put(ClientResponse.class,appSaveTestjson); 
+    	
+    	assertEquals("should return status 201", 201, output.getStatus());
+    }
+    
+    //We are gonna get the app json we saved in the previous save test. 
+    @Test
+    public void appGetTest() throws Exception {
+    	
+    	//Gonna make the GET request.
+    	ClientResponse output = null;
+	output = webResource.path("/appconsole-api/rest/app/b594afad-1e6c-42c4-d58c-715c62cfdfc").get(ClientResponse.class);
+	
+	String appGetTestjson = "";
+	ClassLoader classLoader = getClass().getClassLoader();
+	
+	//Reading JSON File for the put body. 
+	try 
+	{
+		appGetTestjson = IOUtils.toString(classLoader.getResourceAsStream("appGet.json"));
+	} 
+	catch (IOException e) 
+	{
+		//oops something bad happened :(
+		e.printStackTrace();
+		throw e;
+	}
+	
+	if(appGetTestjson == null && appGetTestjson.isEmpty())
+	{
+		//oops something bad happened :(
+		throw new Exception("Unexpected Error while reading appSave json file");
+	}
+	
+	assertEquals("json does not match",appGetTestjson,output.getEntity(String.class));
+    }
+    
 }
--- src/test/java/org/sapphire/appconsole/rest/TestUtils.java
+++ src/test/java/org/sapphire/appconsole/rest/TestUtils.java
@@ -1,39 +1,33 @@
-/**
- * 
- */
 package org.sapphire.appconsole.rest;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 /**
+ * /**
+ * This class is the Utils class for the Junit test cases , 
+ * that has the utility functions.
  * @author deepak
- *
  */
 public class TestUtils {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("The property value is::"+readPropertyFile("baseurl"));
-	}
-	
-	public static String readPropertyFile(String propertyName)
+	public static String readPropertyFile(String propertyName) throws Exception
 	{
 		Properties props = new Properties();
-		InputStream propFile = TestUtils.class.getResourceAsStream("/Users/deepak/work/Sapphire-API/src/main/resources/test.properties");
-		try {
-			props.load(propFile);
-			propFile.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("gonna throw error");
-			e.printStackTrace();
-		}
 		
-		return props.getProperty(propertyName);
-		}
+        try 
+        {
+        			ClassLoader classLoader = TestUtils.class.getClassLoader();
+		    		InputStream propFile = classLoader.getResourceAsStream("test.properties");
+		    		props.load(propFile);
+		    		propFile.close();
+        }
+        catch(Exception e)
+        {
+        		e.printStackTrace();
+        		throw new Exception(e.getMessage());
+        }
+        
+        return props.getProperty(propertyName);
 	}
+}
